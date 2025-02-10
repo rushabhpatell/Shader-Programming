226eb21dcb0cfab3b27090c2f750b64411dc63c9
Rushabh <rushabhpatel109@gmail.com>
2025-02-09 17:53:25 -0800

    Basic triangle that can animate from side to side

---
+++ code/Code.java
@@ -0,0 +1,144 @@
+package code;
+
+import javax.swing.*;
+import static com.jogamp.opengl.GL4.*;
+import com.jogamp.opengl.*;
+import com.jogamp.opengl.awt.GLCanvas;
+import com.jogamp.opengl.GLContext;
+import com.jogamp.opengl.glu.GLU;
+import com.jogamp.common.nio.Buffers;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+import java.util.Vector;
+
+public class Code extends JFrame implements GLEventListener
+{	private GLCanvas myCanvas;
+	private int renderingProgram;
+	private int vao[] = new int[1];
+
+	public Code()
+	{	setTitle("Chapter 2 - program 2");
+		setSize(600, 400);
+		myCanvas = new GLCanvas();
+		myCanvas.addGLEventListener(this);
+		this.add(myCanvas);
+		this.setVisible(true);
+	}
+
+	public void display(GLAutoDrawable drawable)
+	{	GL4 gl = (GL4) GLContext.getCurrentGL();
+		gl.glUseProgram(renderingProgram);
+		gl.glPointSize(1.0f);
+		// gl.glPointSize(30.0f);
+		//gl.glDrawArrays(GL_POINTS,0,1);
+		gl.glDrawArrays(GL_TRIANGLES,0,3);
+	}
+
+	public void init(GLAutoDrawable drawable)
+	{	GL4 gl = (GL4) GLContext.getCurrentGL();
+		renderingProgram = Utils.createShaderProgram("code/vertShader.glsl", "code/fragShader.glsl");
+		gl.glGenVertexArrays(vao.length, vao, 0);
+		gl.glBindVertexArray(vao[0]);
+	}
+
+	private int createShaderProgram()
+	{	GL4 gl = (GL4) GLContext.getCurrentGL();
+
+		String vshaderSource[] = readShaderSource("code/vertShader.glsl");
+		String fshaderSource[] = readShaderSource("code/fragShader.glsl");
+
+		int vShader = gl.glCreateShader(GL_VERTEX_SHADER);
+		int fShader = gl.glCreateShader(GL_FRAGMENT_SHADER);
+
+		gl.glShaderSource(vShader, vshaderSource.length, vshaderSource, null, 0);
+		gl.glCompileShader(vShader);
+
+		gl.glShaderSource(fShader, fshaderSource.length, fshaderSource, null, 0);
+		gl.glCompileShader(fShader);
+
+		int vfprogram = gl.glCreateProgram();
+		gl.glAttachShader(vfprogram, vShader);
+		gl.glAttachShader(vfprogram, fShader);
+		gl.glLinkProgram(vfprogram);
+
+		gl.glDeleteShader(vShader);
+		gl.glDeleteShader(fShader);
+		return vfprogram;
+	}
+
+	public static void main(String[] args) { new Code(); }
+	public void reshape(GLAutoDrawable drawable, int x, int y, int width, int height) {}
+	public void dispose(GLAutoDrawable drawable) {}
+
+	private String[] readShaderSource(String filename)
+	{	Vector<String> lines = new Vector<String>();
+		String[] program;
+		Scanner sc;
+		try
+		{	sc = new Scanner(new File(filename));
+			while (sc.hasNext())
+			{	lines.addElement(sc.nextLine());
+			}
+			program = new String[lines.size()];
+			for (int i = 0; i < lines.size(); i++)
+			{	program[i] = (String) lines.elementAt(i) + "\n";
+			}
+		}
+		catch (IOException e)
+		{	System.err.println("IOException reading file: " + e);
+			return null;
+		}
+		return program;
+	}
+
+	private void printShaderLog(int shader)
+	{	GL4 gl = (GL4) GLContext.getCurrentGL();
+		int[] len = new int[1];
+		int[] chWrittn = new int[1];
+		byte[] log = null;
+
+		// determine the length of the shader compilation log
+		gl.glGetShaderiv(shader, GL_INFO_LOG_LENGTH, len, 0);
+		if (len[0] > 0)
+		{	log = new byte[len[0]];
+			gl.glGetShaderInfoLog(shader, len[0], chWrittn, 0, log, 0);
+			System.out.println("Shader Info Log: ");
+			for (int i = 0; i < log.length; i++)
+			{	System.out.print((char) log[i]);
+			}
+		}
+	}
+
+	void printProgramLog(int prog)
+	{	GL4 gl = (GL4) GLContext.getCurrentGL();
+		int[] len = new int[1];
+		int[] chWrittn = new int[1];
+		byte[] log = null;
+
+		// determine length of the program compilation log
+		gl.glGetProgramiv(prog, GL_INFO_LOG_LENGTH, len, 0);
+		if (len[0] > 0)
+		{	log = new byte[len[0]];
+			gl.glGetProgramInfoLog(prog, len[0], chWrittn, 0, log, 0);
+			System.out.println("Program Info Log: ");
+			for (int i = 0; i < log.length; i++)
+			{	System.out.print((char) log[i]);
+			}
+		}
+	}
+
+	boolean checkOpenGLError()
+	{	GL4 gl = (GL4) GLContext.getCurrentGL();
+		boolean foundError = false;
+		GLU glu = new GLU();
+		int glErr = gl.glGetError();
+		while (glErr != GL_NO_ERROR)
+		{	System.err.println("glError: " + glu.gluErrorString(glErr));
+			foundError = true;
+			glErr = gl.glGetError();
+		}
+		return foundError;
+	}
+}
