904b72daf5e6939e33d4fb4cfd1771df587c59a4
rushabhpatell <159343555+rushabhpatell@users.noreply.github.com>
2025-02-11 23:46:00 -0800

    Update fragShader.glsl

--- code/fragShader.glsl
+++ code/fragShader.glsl
@@ -1,6 +1,16 @@
 #version 430
-out vec4 color;
-void main(void)
+
+in vec3 color;
+out vec4 fragColor;
+uniform int colorMode;
+
+void main()
 {
-	color = vec4(0.0, 0.0, 1.0, 1.0);
+    if (colorMode == 0) {
+        fragColor = vec4(1.0, 1.0, 0.0, 1.0); // Yellow
+    } else if (colorMode == 1) {
+        fragColor = vec4(0.5, 0.0, 0.5, 1.0); // Purple
+    } else {
+        fragColor = vec4(color, 1.0); // Gradient (use vertex colors)
+    }
 }
